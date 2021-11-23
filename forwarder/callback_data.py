796ebec0dad5e49df4ca9f96181ad5082252e0c3
Joke le Sare <87913488+jokeselo@users.noreply.github.com>
2021-11-23 14:04:56 +0530

    Update callback_data.py

--- forwarder/callback_data.py
+++ forwarder/callback_data.py
@@ -1,1 +1,21 @@
+def start(update: Update, _):
+    chat = update.effective_chat
+    message = update.effective_message
+    user = update.effective_user
 
+    if chat.type == "private":
+        message.reply_text(
+            PM_START_TEXT.format(user.first_name, dispatcher.bot.first_name),
+            reply_markup=BUTTONS,
+            parse_mode=ParseMode.HTML,
+        )
+    else:
+        message.reply_text("I'm up and running!")
+
+
+
+
+def main():
+    start_handler = CommandHandler(
+        "start", start,run_async=True
+    )
